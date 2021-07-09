472b464dd37ecce4f0f81440957ae1c841b1530d
hack3r-0m <oparikh113@gmail.com>
2021-07-09 12:28:53 +0530

    new: added staking interfaces

---
+++ contracts/interfaces/IERC900.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.5;
+
+
+/**
+ * @title General Staking Interface
+ *        ERC900: https://eips.ethereum.org/EIPS/eip-900
+ */
+
+interface IERC900 {
+    event Staked(address indexed user, uint256 amount, uint256 total, bytes data);
+    event Unstaked(address indexed user, uint256 amount, uint256 total, bytes data);
+
+    /**
+     * @dev Stake a certain amount of tokens
+     * @param _amount Amount of tokens to be staked
+     * @param _data Optional data that can be used to add signalling information in more complex staking applications
+     */
+    function stake(uint256 _amount, bytes calldata _data) external;
+
+    /**
+     * @dev Stake a certain amount of tokens to another address
+     * @param _user Address to stake tokens to
+     * @param _amount Amount of tokens to be staked
+     * @param _data Optional data that can be used to add signalling information in more complex staking applications
+     */
+    function stakeFor(address _user, uint256 _amount, bytes calldata _data) external;
+
+    /**
+     * @dev Unstake a certain amount of tokens
+     * @param _amount Amount of tokens to be unstaked
+     * @param _data Optional data that can be used to add signalling information in more complex staking applications
+     */
+    function unstake(uint256 _amount, bytes calldata _data) external;
+
+    /**
+     * @dev Tell the current total amount of tokens staked for an address
+     * @param _addr Address to query
+     * @return Current total amount of tokens staked for the address
+     */
+    function totalStakedFor(address _addr) external view returns (uint256);
+
+    /**
+     * @dev Tell the current total amount of tokens staked from all addresses
+     * @return Current total amount of tokens staked from all addresses
+     */
+    function totalStaked() external view returns (uint256);
+
+    /**
+     * @dev Tell the address of the staking token
+     * @return Address of the staking token
+     */
+    function token() external view returns (address);
+
+    /*
+     * @dev Tell if the optional history functions are implemented
+     *      - check interface at IERC900HistoryExtension
+     *
+     * @return True if the optional history functions are implemented
+     */
+    function supportsHistory() external pure returns (bool);
+}
