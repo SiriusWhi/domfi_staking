6a44da4be88d7a830e984ec70ba69f2531461eff
hack3r-0m <oparikh113@gmail.com>
2021-07-10 16:54:25 +0530

    new+refactor: re-structuring of contract and adding initialization to core

---
+++ contracts/utils/Errors.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.5;
+
+abstract contract Errors {
+    string internal constant NOT_ENOUGH_DOM = "NOT_ENOUGH_DOM";
+    string internal constant NOT_A_CONTRACT = "NOT_A_CONTRACT";
+    string internal constant ONLY_OWNER = "ONLY_OWNER";
+    string internal constant REENTRANCY_LOCKED = "REENTRANCY_LOCKED";
+    string internal constant STAKING_NOT_STARTED = "STAKING_NOT_STARTED";
+    string internal constant ZERO_ADDRESS = "ZERO_ADDRESS";
+    string internal constant ZERO_ALLOWANCE = "ZERO_ALLOWANCE";
+    string internal constant ZERO_AMOUNT = "ZERO_AMOUNT";
+}
--- contracts/utils/Modifiers.sol
+++ contracts/utils/Modifiers.sol
@@ -1,23 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.5;
 
+import {Constants} from "./Constants.sol";
+import {Errors} from "./Errors.sol";
 
 /**
  * @title Collection of modifiers instead of using bloated utils
  */
 
-abstract contract Modifiers {
-
-    uint private unlocked = 1;
+abstract contract Modifiers is Constants,Errors {
 
     /**
      * @dev Simple Re-entrancy lock
      */
+    uint256 private unlocked = 1;
     modifier nonReentrant() {
-        require(unlocked == 1, 'REENTRANCY_LOCKED');
+        require(unlocked == 1, REENTRANCY_LOCKED);
         unlocked = 0;
         _;
         unlocked = 1;
+    }
+
+    /**
+     * @dev Only allow owner (i.e contract creator)
+     */
+    address internal owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner, ONLY_OWNER);
+        _;
+    }
+
+    /**
+     * @dev Only allow after staking starts
+     */
+    bool internal stakingStarted;
+    modifier afterStakingStarts() {
+        require(stakingStarted, STAKING_NOT_STARTED);
+        _;
+    }
+
+    function isContract(address _target) internal view returns (bool) {
+        if (_target == address(0)) return false;
+
+        uint256 size;
+
+        assembly { size := extcodesize(_target) }
+        return size > 0;
     }
 
 }
