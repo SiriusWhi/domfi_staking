2cd9fdc333c49db18a505f7adfb976f349008589
SiriusWhi <dprripontipo480@gmail.com>
2021-07-11 15:05:09 +0530

    fix: constants and errors

--- contracts/utils/Constants.sol
+++ contracts/utils/Constants.sol
@@ -6,10 +6,12 @@ abstract contract Constants {
     address internal LP_TOKEN;
     address internal DOM_TOKEN;
 
-    uint256 internal constant STAKING_PERIOD = 7 days;
-    uint256 internal constant PENALTY_PERIOD = 7 days;
-    uint256 internal constant EARNING_PERIOD = 120 days;
+    uint256 internal STAKING_START_TIMESTAMP;
 
-    uint256 internal TOTAL_DOM;
+    uint256 internal constant STAKING_PERIOD = 7 days;
+    uint256 internal constant REWARD_PERIOD = 120 days;
+    uint256 internal constant LSP_PERIOD = 365 days;
+
+    uint256 public TOTAL_DOM;
 
 }
