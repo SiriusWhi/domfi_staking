4350d5c5f72339c2da4a1aa072624e0efa8a6f58
SiriusWhi <dprripontipo480@gmail.com>
2021-07-14 00:57:57 +0530

    fix: rewards ratio in propotion to stake, better comments

--- contracts/utils/Errors.sol
+++ contracts/utils/Errors.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.5;
 abstract contract Errors {
     string internal constant NOT_ENOUGH_DOM = "NOT_ENOUGH_DOM";
     string internal constant NOT_ENOUGH_ALLOWANCE = "NOT_ENOUGH_ALLOWANCE";
+    string internal constant NOT_ENOUGH_STAKE = "NOT_ENOUGH_STAKE";
     string internal constant NOT_A_CONTRACT = "NOT_A_CONTRACT";
     string internal constant ONLY_OWNER = "ONLY_OWNER";
     string internal constant REENTRANCY_LOCKED = "REENTRANCY_LOCKED";
